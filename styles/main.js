0d6d2c3d45be6f6a28225be72c3edb60d96bd764
bscannestra <bsakuracannestra@gmail.com>
2023-04-27 17:31:42 -0700

    Animation in JS rather than Gifs

---
+++ styles/main.js
@@ -0,0 +1,30 @@
+const totalFrames = 18;
+const animationDuration = 1300;
+const timePerFrame = animationDuration / totalFrames;
+let timeWhenLastUpdate;
+let timeFromLastUpdate;
+let frameNumber = 1;
+
+function step(startTime) {
+    if (!timeWhenLastUpdate) timeWhenLastUpdate = startTime;
+    timeFromLastUpdate = startTime - timeWhenLastUpdate;
+
+    if (timeFromLastUpdate > timePerFrame) {
+        // hide all frames
+        $('.gamerplaying').css('opacity', 0);
+        $(`.playing-${frameNumber}`).css('opacity', 1);
+        timeWhenLastUpdate = startTime;
+
+        if (frameNumber >= totalFrames) {
+            frameNumber = 1;
+          } else {
+            frameNumber = frameNumber + 1;
+          }
+    }
+
+    requestAnimationFrame(step);
+}
+
+$(window).on('load', () => {
+    requestAnimationFrame(step);
+  });
